fe3883b1ccfea140eba1407eae6922378f8ce2dc
SuperPracion <superpracion2@gmail.com>
2025-06-24 19:25:23 +0300

    Added is_double click Tests

---
+++ tests/is_double_click_test.py
@@ -0,0 +1,18 @@
+from microbutton_core.button import Button, PRESS, UNPRESS
+
+# Default parameters
+button = Button()
+assert button.is_click == False
+# Click
+button.state = PRESS
+button.state = UNPRESS
+# Click
+button.state = PRESS
+button.state = UNPRESS
+assert button.is_click == False
+assert button.is_double_click == True
+assert button.is_multi_click == False
+assert button.is_long_press == False
+assert button.click_count == 2
+assert button.last_press_time != -1
+assert button.state == UNPRESS
