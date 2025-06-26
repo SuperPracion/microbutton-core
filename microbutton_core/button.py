2df5f7d4bef5da2d91419d9dcb30fac186ab8252
SuperPracion <superpracion2@gmail.com>
2025-06-26 21:19:42 +0300

    python time to micropython time

--- microbutton_core/button.py
+++ microbutton_core/button.py
@@ -2,7 +2,7 @@ __all__ = ["Button"]
 __version__ = "1.0.0"
 __author__ = "Akhmetov Timur Elmirovich"
 
-from time import time
+from time import time_ms
 
 # PRESS - 0
 # UNPRESS - 1
@@ -41,7 +41,7 @@ class Button:
 
     @state.setter
     def state(self, value: int):
-        now = time()
+        now = time_ms()
         if value == self.press_value:
             if (
                 self._state == PRESS
